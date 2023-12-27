c8c615db1dc1282fc92806b8b2c176d6b1d43525
Anton <anton.zhitomirsky@gmail.com>
2023-12-27 15:26:23 +0000

    cleanup script adn descrition of source data

---
+++ research/notes/cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+find . -type f -name "*.aux" | xargs rm
+find . -type f -name "*.fdb_latexmk" | xargs rm
+find . -type f -name "*.fls" | xargs rm
+find . -type f -name "*.log" | xargs rm
+find . -type f -name "*.synctex.gz" | xargs rm
+find . -type f -name "*.out" | xargs rm
