713500e7bcfcf72951431bd7ac90bc4f69a647a2
Anton <anton.zhitomirsky@gmail.com>
2024-01-02 19:52:24 +0000

    setup of totalsegmentator research playground

---
+++ research/source/code/totalsegmentator/set-up-environment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# obtain path to temporary directory to hold python's virtual environment
+currPath=$(pwd)
+venvPath=$currPath/../tmp/totseg-env
+echo "DEBUG: creating venv path at ${venvPath}"
+mkdir -p $venvPath
+
+# create virtual environment
+export PENV=$venvPath
+python3 -m virtualenv $PENV
+
+# activate virtual environment
+source $PENV/bin/activate
+echo "verify the following are pointing to the correct location"
+echo "       `which pip`"
+echo "       `which python`"
+echo "       `which python3`"
+
+# install requirements from requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "================"
+echo "Enter the virtual environment with the following command:"
+echo "source ../tmp/totseg-env/bin/activate"
+echo "(exit with command 'disconnect')"
+echo "================"
