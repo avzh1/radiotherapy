5ee829154ff8cc483980f995dede40a0d280abc9
Anton <anton.zhitomirsky@gmail.com>
2024-01-09 15:59:35 +0000

    view metadata of nii.gz file

--- research/source/code/set-up-environment.sh
+++ research/source/code/set-up-environment.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ $# != 1 ]
+then
+    echo "Please provide the name of the directory from which you want to create a python venv for"
+fi
+
+DIR=$1
+
 # obtain path to temporary directory to hold python's virtual environment
 currPath=$(pwd)
-venvPath=$currPath/../tmp/totseg-env
+venvPath=$currPath/tmp/$DIR
 echo "DEBUG: creating venv path at ${venvPath}"
 mkdir -p $venvPath
 
@@ -19,10 +26,10 @@ echo "       `which python3`"
 
 # install requirements from requirements.txt
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r $DIR/requirements.txt
 
 echo "================"
 echo "Enter the virtual environment with the following command:"
-echo "source ../tmp/totseg-env/bin/activate"
+echo "source tmp/$DIR/bin/activate"
 echo "(exit with command 'deactivate')"
 echo "================"
