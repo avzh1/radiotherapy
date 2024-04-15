816cbc3085d5109bfe320d49d82abe20d4e14ba8
Anton Zhitomirsky <az620@ic.ac.uk>
2024-04-15 16:03:00 +0100

    visualising the slices for qualitative measure

--- research/source/code/nnUNet/1_training/nnUnet_train_50.sh
+++ research/source/code/nnUNet/1_training/nnUnet_train_50.sh
@@ -5,8 +5,13 @@
 #SBATCH -p gpus                     # Partition (queue)
 #SBATCH --gres gpu:1                # gpu:n, where n = number of GPUs
 #SBATCH --mem 12288                 # memory pool for all cores
-#SBATCH --nodelist monal03          	# SLURM node
 #SBATCH --output=slurm.%N.%j.log    # Standard output and error log
+
+# Check if $1 variable is empty
+if [ -z "$1" ]; then
+    echo "Error: DATASET_NAME_OR_ID is not provided."
+    return -1
+fi
 
 # Source virtual environment (pip)
 source /vol/bitbucket/az620/radiotherapy/.venv/bin/activate
@@ -15,4 +20,9 @@ source /vol/bitbucket/az620/radiotherapy/.venv/bin/activate
 source /vol/bitbucket/az620/radiotherapy/data/data_vars.sh
 
 # Run python script
-nnUNetv2_train 1 3d_fullres 2
+# nnUNetv2_train DATASET_NAME_OR_ID UNET_CONFIGURATION FOLD [additional options, see -h]
+nnUNetv2_train $1 3d_fullres 0 -tr nnUNetTrainer_50epochs --npz
+nnUNetv2_train $1 3d_fullres 1 -tr nnUNetTrainer_50epochs --npz
+nnUNetv2_train $1 3d_fullres 2 -tr nnUNetTrainer_50epochs --npz
+nnUNetv2_train $1 3d_fullres 3 -tr nnUNetTrainer_50epochs --npz
+nnUNetv2_train $1 3d_fullres 4 -tr nnUNetTrainer_50epochs --npz
