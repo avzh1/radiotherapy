e734e18c8917a875543f8910796f601cc1ca120b
Anton Zhitomirsky <az620@ic.ac.uk>
2024-05-06 11:17:25 +0100

    data_vars.sh

---
+++ data/data_vars.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# run with . data_vars.sh
+
+# get the directory of the script
+DATA_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Create an empty array to store the new environment variables
+new_env_vars=()
+
+# Get names of all directories present in DATA_DIR and export them
+for dir in $(find $DATA_DIR -maxdepth 1 -type d ! -name "$(basename $DATA_DIR)"); do
+    export $(basename $dir)=$dir
+    new_env_vars+=($(basename $dir))
+done
+
+# assume we have an nnUNet_raw folder. Therefore, inside we have the different organ
+# classes. Get names of all directories present in nnUNet_raw and export them
+for dir in $(find $nnUNet_raw -maxdepth 1 -type d ! -name "$(basename $nnUNet_raw)"); do
+    IFS="_" read -ra parts <<< "$(basename $dir)"
+    # access the parts of the string using ${parts[index]}
+    export ${parts[1]}=$dir
+    # add new environment variable to list
+    new_env_vars+=(${parts[1]})
+done
+
+# images and ground truth label directories
+export data_trainingImages=imagesTr
+export data_trainingLabels=labelsTr
+new_env_vars+=(data_trainingImages data_trainingLabels)
+
+# check if the script is called with an argument indicating we should print the
+# environment variables
+if [ -n "$1" ]; then
+    # Iterate over the array and export the new environment variables
+    for var in "${new_env_vars[@]}"; do
+        echo "$var: ${!var}"
+    done
+fi
