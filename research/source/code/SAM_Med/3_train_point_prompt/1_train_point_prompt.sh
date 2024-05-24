76720d354a9ea038390b23cecc1d93c4c0f182b4
Anton Zhitomirsky <az620@ic.ac.uk>
2024-05-24 12:07:59 +0100

    Medsam caching

---
+++ research/source/code/SAM_Med/3_train_point_prompt/1_train_point_prompt.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Example of running python script in a batch mode
+#SBATCH -c 16                            # Number of CPU Cores
+#SBATCH -p gpus                          # Partition (queue)
+#SBATCH --gres gpu:1                     # gpu:n, where n = number of GPUs
+#SBATCH --mem 20G                        # memory pool for all cores
+#SBATCH --output=logs/slurm.%N.%j.log    # Standard output and error log
+
+#SBATCH --job-name=MEDSAM.finetune
+
+# --nodelist lory           # SLURM node
+# --nodelist loki          	# SLURM node
+
+# Get the directory of the script
+SOURCE_DIR=$(git rev-parse --show-toplevel)
+
+# Check if $1 variable is empty
+if [ -z "$1" ]; then
+    echo "Error: ANATOMY_NAME is not provided."
+    return -1
+fi
+
+# Load the virtual environment
+source ${SOURCE_DIR}/.venv/bin/activate
+# source ${SOURCE_DIR}/data/data_vars.sh
+
+# Convert Python Script
+jupyter nbconvert --to script '1_train_point_prompt.ipynb'
+
+# Run python script
+python3 1_train_point_prompt.py --anatomy $1 --model_training points --epochs 100 --batch_size=6
